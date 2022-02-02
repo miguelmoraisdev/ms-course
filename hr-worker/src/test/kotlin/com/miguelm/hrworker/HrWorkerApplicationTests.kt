dd5f485f53704729876eb84726b4b0ba5dcc4a65
miguelmoraisdev <miguelmoraisdev@gmail.com>
2022-02-02 16:48:40 -0300

    Project hr-woerker created

---
+++ hr-worker/src/test/kotlin/com/miguelm/hrworker/HrWorkerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miguelm.hrworker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
